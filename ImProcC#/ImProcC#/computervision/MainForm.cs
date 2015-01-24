4e3f50c56f564253889d8e7c2bebb281b1e8ba96
raresh7 <raresroman7@yahoo.com>
2015-01-24 20:41:46 +0200

    Revert "am dat enter"
    This reverts commit 2f3c109f244b97daca3625279893f8089a19699c.

--- ImProcC#/ImProcC#/computervision/MainForm.cs
+++ ImProcC#/ImProcC#/computervision/MainForm.cs
@@ -34,7 +34,6 @@ namespace ComputerVision
             image = new Bitmap(sSourceFileName);
             workImage = new FastImage(image);
             workImage1 = new FastImage(image);
-
             pixels = new Color[workImage1.Width, workImage1.Height];
         }
 
